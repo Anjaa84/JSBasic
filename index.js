0742e2a86c12088aaed7997007478997050c2f10
Manikavasagar Anjanan <it18186048@my.sliit.lk>
2021-12-28 15:14:33 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,131 @@
+// let person = {
+//     name: 'Mosh',
+//     age: 30
+// };
+
+// // dot notation
+// person.name = ' John';
+
+// // Bracket notation
+// let selection = 'name'
+// person[selection] = 'Mary';
+
+// console.log(person);
+
+
+// let colorArray = ['red', 'green', 'blue'];
+// console.log(colorArray.length)
+
+
+// function greet(firstName, lastName) {
+//     console.log('Hi ' + firstName + ' ' + lastName);
+// }
+
+
+// greet('Anjanan', 'Maniks');
+
+
+// function sqaure(num) {
+//     return num * num;
+// }
+
+// console.log(sqaure(10));
+
+
+// function reverseString(word) {
+//     let reverse;
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         reverse = reverse + word[i];
+//     }
+//     console.log(reverse)
+
+// }
+
+// metrics = ['performance', 'security', 'iam']
+
+// console.log('metrices are' + metrics[2]);
+
+
+// let gcpServer = {
+//     address: "Google",
+//     number: 12,
+//     metric: {
+//         performance: "high",
+//         affected: 20,
+//         totalNoOfMachines: 35
+//     },
+//     dataUsage: "  High",
+//     isActive: false,
+//     activeUsage: null,
+//     ownerName: "Jagath"
+
+
+// }
+
+
+// function createServerConnection(ghajini) {
+//     let db = 'MySQL'
+//     if (ghajini == "memoryloss") {
+//         console.log("hit in the head");
+//     }
+
+//     return 7;
+
+// }
+
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// add(3, 4);
+
+// if (gcpServer.number > 40) {
+//     console.log("Reached the max value")
+// } else {
+//     console.log("On limit")
+// }
+
+// console.log(gcpServer.metric.affected);
+
+// function reverseString(str) {
+
+//     var newString = "";
+
+
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         newString += str[i]; // or newString = newString + str[i];
+//     }
+
+
+//     return newString;
+// }
+
+
+
+// let words = reverseString("hello");
+
+
+// console.log(words))
+
+
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+
+// }
+// let x = 'Anjanan';
+
+// console.log(x !== 'Anjanan');
+
+// Java will convert it for you, so there is no need of a worry in javascript
+
+
+// console.log('Cnjanan' < 'Banu');
+
+console.log(1 == true);
+
+
+
